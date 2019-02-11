a48a435dce20f2b6a860fe34f6badf3666544710
FabioOrtiz <42221899+FabioOrtiz@users.noreply.github.com>
2019-02-11 15:53:35 -0500

    Update greetingEnum.py

--- greetingEnum.py
+++ greetingEnum.py
@@ -1,8 +1,16 @@
 from enum import IntEnum
 
+
 class greetingenum(IntEnum):
+
+    # To each expression, i assigned a number
+
     GoodMorning =1
     BuenosDias =2
     BomDia = 3
     Ohayo = 4
     Boungiorno = 5
+
+    # The only one who has not a module is German
+
+    GutenMorgen = 6
