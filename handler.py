a0d5295d3cca7bdd3168e2203327b6429a759efe
FabioOrtiz <42221899+FabioOrtiz@users.noreply.github.com>
2019-02-11 15:54:11 -0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,12 +1,18 @@
 import abc
 
+# The abstract class for each handler
+
+
 class Handler(object):
+
     def __init__(self):
         self.nextHandler = None
 
     @abc.abstractmethod
     def handle(self, request):
         pass
+
+    # The method of assign the following handler
 
     def next_handle(self, handler):
         self.nextHandler = handler
