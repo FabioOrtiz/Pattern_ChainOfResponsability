ebe79d1b9d40509c9cf10799f43c1083b92aadea
FabioOrtiz <42221899+FabioOrtiz@users.noreply.github.com>
2019-02-11 15:52:53 -0500

    Update chain.py

--- chain.py
+++ chain.py
@@ -4,6 +4,9 @@ from languages import EnglishHandler, SpanishHandler, JapaneseHandler, ItalianHa
 class Chain:
 
     def set_chain(self, greetingenum):
+
+        # Here i create every handler than i use
+
         english_handler = EnglishHandler()
         spanish_handler = SpanishHandler()
         portuguese_handler = PortugueseHandler()
@@ -11,10 +14,17 @@ class Chain:
         italian_handler = ItalianHandler()
         error_handler = ErrorHandler()
 
+        # Assign the next one handler to each handler
+
         english_handler.next_handle(spanish_handler)
         spanish_handler.next_handle(portuguese_handler)
         portuguese_handler.next_handle(japanese_handler)
         japanese_handler.next_handle(italian_handler)
+
+        # Italian is the last one of the chain, by that i assigned to Error Handler
+
         italian_handler.next_handle(error_handler)
+
+        # This is the start of the responsibility chain
 
         english_handler.handle(greetingenum)
