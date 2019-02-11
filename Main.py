3ea5a6a73a5949a8df2c77b2ae64c3825686873d
FabioOrtiz <42221899+FabioOrtiz@users.noreply.github.com>
2019-02-11 15:40:32 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,19 @@ from greetingEnum import greetingenum
 from chain import Chain
 
 if __name__ == '__main__':
-    #chain = set_chain(GreetingEnum.GutenVag)
+
+    # Inicialization of the chain
+
     chain = Chain()
+
+    # Set of some examples
 
     chain.set_chain(greetingenum.BuenosDias)
     chain.set_chain(greetingenum.GoodMorning)
     chain.set_chain(greetingenum.BomDia)
     chain.set_chain(greetingenum.Boungiorno)
     chain.set_chain(greetingenum.Ohayo)
+
+    # The next one doesnt belong to any module
+
+    chain.set_chain(greetingenum.GutenMorgen)
