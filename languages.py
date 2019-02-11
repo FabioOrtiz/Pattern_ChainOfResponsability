cca99f230cdc766483567686a57e99ef0e313463
FabioOrtiz <42221899+FabioOrtiz@users.noreply.github.com>
2019-02-11 15:54:48 -0500

    Update languages.py

--- languages.py
+++ languages.py
@@ -1,16 +1,16 @@
 import handler
 from greetingEnum import greetingenum
 
-
-class ErrorHandler(handler.Handler):
-    def handle(self):
-        print("The Language was not found")
+# This package is composed of every language than is used
 
 
 class EnglishHandler(handler.Handler):
+
+    # The comprobation of the Enum
+
     def handle(self, request):
         if request is greetingenum.GoodMorning:
-            print ("Hi! How are you?")
+            print("Hi! How are you?")
         else:
             if self.nextHandler is not None:
                 self.nextHandler.handle(request)
@@ -19,7 +19,7 @@ class EnglishHandler(handler.Handler):
 class ItalianHandler(handler.Handler):
     def handle(self, request):
         if request is greetingenum.Boungiorno:
-            print ("Ciao! ¿Come stai?")
+            print("Ciao! ¿Come stai?")
         else:
             if self.nextHandler is not None:
                 self.nextHandler.handle(request)
@@ -28,7 +28,7 @@ class ItalianHandler(handler.Handler):
 class JapaneseHandler(handler.Handler):
     def handle(self, request):
         if request is greetingenum.Ohayo:
-            print ("Konichiwa! Ogenkidesuka?")
+            print("Konichiwa! Ogenkidesuka?")
         else:
             if self.nextHandler is not None:
                 self.nextHandler.handle(request)
@@ -37,7 +37,7 @@ class JapaneseHandler(handler.Handler):
 class PortugueseHandler(handler.Handler):
     def handle(self, request):
         if request is greetingenum.BomDia:
-            print ("Ola! ¿Como Vai?")
+            print("Ola! ¿Como Vai?")
         else:
             if self.nextHandler is not None:
                 self.nextHandler.handle(request)
@@ -46,7 +46,14 @@ class PortugueseHandler(handler.Handler):
 class SpanishHandler(handler.Handler):
     def handle(self, request):
         if request is greetingenum.BuenosDias:
-            print ("Hola! ¿Como estas?")
+            print("Hola! ¿Como estas?")
         else:
             if self.nextHandler is not None:
                 self.nextHandler.handle(request)
+
+
+# This is the last handler, the last one of the chain
+
+class ErrorHandler(handler.Handler):
+    def handle(self, request):
+        print("The Language was not found")
